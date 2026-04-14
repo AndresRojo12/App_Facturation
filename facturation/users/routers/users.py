140a5039c3ebecc6f09ca01a1fcc47426e72cb8a
Andres <andresrojo20013@gmail.com>
2026-04-14 13:11:10 -0500

    se importa modelo de respuesta user #3

--- facturation/users/routers/users.py
+++ facturation/users/routers/users.py
@@ -1,10 +1,10 @@
 from typing import List
 
 from fastapi import APIRouter, Depends
-from facturation.users.schemas.user_schema import UserCreate
+from facturation.users.schemas.user_schema import UserCreate, UserResponse
 
 router = APIRouter()
 
-@router.post("/users")
+@router.post("/users", response_model=UserResponse)
 async def create_user(user: UserCreate):
     return user
