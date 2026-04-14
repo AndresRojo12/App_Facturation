c66421ce83b9fca16b2f340dd95b381cba42524a
Andres <andresrojo20013@gmail.com>
2026-04-14 14:51:45 -0500

    se crea y se importa dependencia de base de datos #3

--- facturation/users/routers/users.py
+++ facturation/users/routers/users.py
@@ -1,10 +1,12 @@
 from typing import List
 
 from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from facturation.database.dependencies.dependencie_session import SessionDep
 from facturation.users.schemas.user_schema import UserCreate, UserResponse
 
 router = APIRouter()
 
 @router.post("/users", response_model=UserResponse)
-async def create_user(user: UserCreate):
+async def create_user(user: UserCreate, db: Session = Depends(SessionDep)):
     return user
