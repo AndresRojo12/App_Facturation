7d09486c925646fe24a78409f5456ace723e65fe
Andres <andresrojo20013@gmail.com>
2026-04-14 12:35:35 -0500

    se crea ruta post para users #3

--- facturation/users/routers/users.py
+++ facturation/users/routers/users.py
@@ -1,39 +1,10 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
-from sqlmodel import select
-
-from facturation.database.dependencies.dependencie_session import SessionDep
-from facturation.users.models.user_model import User, UserCreate
+from fastapi import APIRouter, Depends
+from facturation.users.schemas.user_schema import UserCreate
 
 router = APIRouter()
 
-
-@router.get("/users", response_model=List[User])
-def get_users(session: Session = Depends(SessionDep)):
-    users = session.exec(select(User)).all()
-    return users
-
-
-@router.post("/users", response_model=User, status_code=status.HTTP_201_CREATED)
-def create_user(user_in: UserCreate, session: Session = Depends(SessionDep)):
-    existing = session.exec(select(User).where(User.email == user_in.email)).first()
-    if existing:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="A user with this email already exists.",
-        )
-    user = User(email=user_in.email, password=user_in.password)
-    session.add(user)
-    try:
-        session.commit()
-        session.refresh(user)
-    except IntegrityError:
-        session.rollback()
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="Unable to create user; email might already be registered.",
-        )
+@router.post("/users")
+async def create_user(user: UserCreate):
     return user
