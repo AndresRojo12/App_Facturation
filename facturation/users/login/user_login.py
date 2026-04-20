45c4ba7fbe3773a793cec270bf3501a6cd150a77
Andres <andresrojo20013@gmail.com>
2026-04-20 11:40:58 -0500

    se trae el user a traves del token #2

--- facturation/users/login/user_login.py
+++ facturation/users/login/user_login.py
@@ -3,13 +3,14 @@ from fastapi import FastAPI, Depends
 from facturation.users.schemas.user_schema import UserResponse
 from fastapi.security import OAuth2PasswordBearer
 
+from facturation.users.services.user_service import get_user
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def fake_decode_token(token):
-    return UserResponse(
-        username=token
-    )
+    user = get_user(token)
+    return user
 
 # oauth2_schem se usa en una dependencia con Depends
 def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
