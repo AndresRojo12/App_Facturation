29e24427fc1d72dccc831899ad5693738e20baaa
Andres <andresrojo20013@gmail.com>
2026-04-28 12:24:43 -0500

    se importa configuracion de variables de entorno a main #2

--- facturation/main.py
+++ facturation/main.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI, Depends
 from facturation.products.routers import products
 from facturation.users.routers import users
 from facturation.users.schemas.user_schema import UserCreate, UserResponse
+from facturation.core.config import settings
 
-app = FastAPI()
+app = FastAPI(title=settings.APP_NAME)
 app.include_router(products.router)
 app.include_router(users.router)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"app_name": settings.APP_NAME}
