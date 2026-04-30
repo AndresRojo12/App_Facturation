42fbec0118890b4ea43c52fb8fd28d5060eebe04
Andres <andresrojo20013@gmail.com>
2026-04-30 14:02:33 -0500

    se crea modelo de product a base de datos #5

---
+++ facturation/products/models/product_model.py
@@ -0,0 +1,15 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import Boolean, Column, DateTime, Float, Integer, String
+from facturation.database.base import Base
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    price = Column(Float, nullable=False)
+    stock = Column(Integer, nullable=False)
+    activo = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
