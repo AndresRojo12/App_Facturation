b1597992e8ff690b2793ea2e80233d4f316215c3
Andres <andresrojo20013@gmail.com>
2026-04-30 14:56:38 -0500

    se crea ruta post para productos #7

--- facturation/products/routers/products.py
+++ facturation/products/routers/products.py
@@ -1,7 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from alembic.util import status
+from facturation.database.dependencies.dependencie_session import SessionDep
+from facturation.products.schemas.product_schema import ProductCreate, ProductResponse
+from facturation.products.services.product_service import create_product, get_product
+
 
 router = APIRouter()
+
+# create routes http for products
 
 @router.get("/products")
 async def get_products():
     return { "message": "List of products" }
+
+@router.post("/products")
+async def create_new_product(product: ProductCreate, db: SessionDep):
+    return await create_product(product, db)
