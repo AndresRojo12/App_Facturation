20b2d07b3df5f04692adfbc492914a4be9819f68
Andres <andresrojo20013@gmail.com>
2026-05-05 11:40:07 -0500

    se crea modelo de venta #13

---
+++ facturation/sales/models/sale_model.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import Boolean, Column, DateTime, Float, ForeignKey, Integer
+from facturation.database.base import Base
+
+class Sale(Base):
+    __tablename__ = "sales"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    total_price = Column(Float, nullable=False)
+    activo = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
