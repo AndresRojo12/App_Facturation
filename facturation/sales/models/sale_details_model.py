1a8c9646fd2f3cfbab7230f3d6de1cc729a59725
Andres <andresrojo20013@gmail.com>
2026-05-06 12:55:21 -0500

    se crea modelo de migración, base de datos #14

---
+++ facturation/sales/models/sale_details_model.py
@@ -0,0 +1,16 @@
+from datetime import datetime, timezone
+
+from sqlalchemy import Boolean, Column, DateTime, Float, ForeignKey, Integer
+from facturation.database.base import Base
+
+class SaleDetails(Base):
+    __tablename__ = "sale_details"
+
+    id = Column(Integer, primary_key=True, index=True)
+    sale_id = Column(Integer, ForeignKey("sales.id"), nullable=False)
+    product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    price = Column(Float, nullable=False)
+    subtotal = Column(Float, nullable=False)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
